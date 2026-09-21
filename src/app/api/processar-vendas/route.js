4d53002bdc6327cbd2f35758eeb686535a484f7a
BestFit001 <felipe.camargo@usebestfit.com.br>
2026-09-21 16:26:25 -0300

    remove temporariamente rota com erro de validacao

--- src/app/api/processar-vendas/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(request) {
-  try {
-    return NextResponse.json({ sucesso: true, mensagem: "API processada com sucesso." });
-  } catch (erro) {
-    return NextResponse.json({ erro: 'Erro interno.' }, { status: 500 });
-  }
-}
