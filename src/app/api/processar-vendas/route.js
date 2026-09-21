ba5bf1c823c430cecb262899cda36e182e3b43c4
BestFit001 <felipe.camargo@usebestfit.com.br>
2026-09-21 16:21:19 -0300

    corrige rota api para javascript puro

--- src/app/api/processar-vendas/route.js
+++ src/app/api/processar-vendas/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
+export async function POST(request) {
   try {
     return NextResponse.json({ sucesso: true, mensagem: "API processada com sucesso." });
   } catch (erro) {
